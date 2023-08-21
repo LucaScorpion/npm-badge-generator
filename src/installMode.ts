1731c3e38c8b7a353f02d245f03bbd9b2b453110
Thomas Biesaart <thomas.biesaart@protonmail.com>
2023-08-21 21:24:58 +0200

    Add the yarn mode (#12)

--- src/installMode.ts
+++ src/installMode.ts
@@ -1,6 +1,6 @@
-export type InstallMode = 'install' | 'global' | 'npx';
+export type InstallMode = 'install' | 'global' | 'npx' | 'yarn';
 
-const validModes: InstallMode[] = ['install', 'global', 'npx'];
+const validModes: InstallMode[] = ['install', 'global', 'npx', 'yarn'];
 
 export function parseInstallMode(mode?: unknown): InstallMode | undefined {
   if (typeof mode === 'string' && validModes.includes(mode as InstallMode)) {
